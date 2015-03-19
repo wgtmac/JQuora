01d31ee63909f8a30a6c75c5079600be3f08d38d
dgzzhb <TianZheng@cmu.edu>
2015-03-18 22:07:05 -0400

    Initialize project

---
+++ src/JQuora.java
@@ -0,0 +1,19 @@
+/**
+ * This is an unofficial Quora API written in Java.
+ * @author Tian Zheng NaitGnehz@gmail.com
+ * @author Gang Wu ustcwg@gmail.com
+ * Carnegie Mellon University
+ * March 18th, 2015
+ */
+
+public class JQuora {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
