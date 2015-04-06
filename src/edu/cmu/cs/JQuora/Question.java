ecb855e23987761234b9aed7c3ea6c42bb20a95c
dgzzhb <TianZheng@cmu.edu>
2015-04-06 18:13:51 -0400

    Question attribute finished

--- src/edu/cmu/cs/JQuora/Question.java
+++ src/edu/cmu/cs/JQuora/Question.java
@@ -14,13 +14,28 @@ import java.util.HashMap;
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
 import org.jsoup.nodes.Element;
+import org.jsoup.select.Elements;
 
 public class Question {
 	String url;
+	HashMap<String, String> question;
 	
-	public Question(String question) {
-		this.url = new String("https://www.quora.com/" + question);
+	public Question(String name) throws IOException {
+		this.url = new String("https://www.quora.com/" + name);
+		this.question = new HashMap<String, String>();
 		
+		Document doc = Jsoup.connect(url).get();
+		// Parse title
+		String title = doc.title();
+		title = title.substring(0, title.length() - 8);
+		question.put("title", title);
+		
+		// Parse want answers 
+		Element QuestionFooter = doc.select("div.action_bar_inner").first();
+		String wantAnswer = QuestionFooter.select("span.count").first().text();
+//		// Seperate digits and letters
+//		wantAnswer = wantAnswer.split("(?<=\\D)(?=\\d)|(?<=\\d)(?=\\D)")[1];
+		question.put("wantAnswer", wantAnswer);
 	}
 	
 	// Get Answer from Specific Author
@@ -38,29 +53,35 @@ public class Question {
 		Element answerText = doc.select("div.answer_text").first();
 		Element answerContent = answerText.select("div.answer_content").first();
 		Element contentFooter = answerContent.select("div.AnswerFooter").first();
-		Element answerFirstFooter = doc.select("div.action_bar_inner").first();
-		Element answerSecondFooter = doc.select("div.action_bar_inner").last();
-
+		Element answerFooter = doc.select("div.action_bar_inner").last();
+		
+		
 		// Content
 		String contentText = answerContent.text();
 		String answerUrl = "https://www.quora.com" + contentFooter.select("a.answer_permalink").first().attr("href");
 		//Update View
-		
-		// Footer
-		String wantAnswer = answerFirstFooter.select("div.primary_item").text();
+
 		
 		
 		//return doc.toString();
 		//return answerText.toString();
 		//return answerContent.toString();
-		//return answerFirstFooter.toString();
-		return wantAnswer;
+		return answerFooter.toString();
 		//return answerUrl;
 		
 	}
 
+	
+	public HashMap<String, String> getQuestion() {
+		return question;
+	}
+
+	public void setQuestion(HashMap<String, String> question) {
+		this.question = question;
+	}
+
 	public static void main(String[] args) throws IOException {
-		Question question = new Question("Are-all-the-U-S-Presidents-related-to-each-other");
+		Question quest = new Question("Are-all-the-U-S-Presidents-related-to-each-other");
 		System.out.println(question.getAnswerByAuthor("Brian-Roemmele"));
 	}
 }
